4ef57f0ea07035a641e30699c5a233feb3819e21
João Fellipe Uller <30800388+joaofel-u@users.noreply.github.com>
2018-05-11 13:40:11 -0300

    Update aeroporto.c

--- aero-sim-master/aeroporto.c
+++ aero-sim-master/aeroporto.c
@@ -1,91 +1,103 @@
-#include "aeroporto.h"
+#ifndef AEROPORTO_H
+#define AEROPORTO_H
+
+#include <semaphore.h>
+#include "unistd.h"		// para uso de tempo em microssegundos
+#include "stdlib.h"
+#include "stdio.h"
+#include "aviao.h"
+#include "fila.h"
+
+#define MICRO_TO_MILI 1000
+
+typedef size_t tempo_t;
+
+typedef struct {
+	size_t n_pistas;
+	size_t n_portoes;
+	size_t n_esteiras;
+	size_t n_max_avioes_esteira;
+	tempo_t t_pouso_decolagem;
+	tempo_t t_remover_bagagens;
+	tempo_t t_inserir_bagagens;
+	tempo_t t_bagagens_esteira;
+
+	// ADICIONADOS
+	pthread_t thread;
+	fila_ordenada_t *fila_pouso_decolagem;
+	fila_ordenada_t *fila_prioritaria;
+	sem_t pistas;
+	sem_t portoes;
+	sem_t esteiras;
+	sem_t n_avioes;
+	// Adicionar aqui outros atributos que você achar necessários.
+	// Exemplo: esteiras, portões, etc...
+} aeroporto_t;
+
 
 /**
- * aeroporto.c
- * Implementação das funções do aeroporto.h
- * Descrições em aeroporto.h
+ * Esta função deve fazer a alocação dinâmica de um aeroporto (malloc)
+ * e atribuir os parâmetros (recebidos por um array de unsigned ints)
+ * aos atributos da struct aeroporto
  **/
+aeroporto_t* iniciar_aeroporto (size_t* args, size_t n_args);
 
-aeroporto_t* iniciar_aeroporto (size_t* args, size_t n_args) {
+/**
+ * Esta função deve ser chamada quando um novo avião se aproxima
+ * do aeroporto. Nesta situação um avião deve pousar em seguida,
+ * mas somente se houver uma pista livre para ele.
+ **/
+void aproximacao_aeroporto (aeroporto_t* aeroporto, aviao_t* aviao);
 
-	aeroporto_t *a = (aeroporto_t *) malloc(sizeof(aeroporto_t));
-	a->n_pistas = args[0];
-	a->n_portoes = args[1];
-	a->n_esteiras = args[2];
-	a->n_max_avioes_esteira = args[3];
-	a->t_pouso_decolagem = args[4];
-	a->t_remover_bagagens = args[5];
-	a->t_inserir_bagagens = args[6];
-	a->t_bagagens_esteira = args[7];
-	a->fila_pouso_decolagem = criar_fila();
-	a->fila_prioritaria = criar_fila();
-	sem_init(&a->pistas, 0, a->n_pistas);
-	sem_init(&a->portoes, 0, a->n_portoes);
-	sem_init(&a->esteiras, 0, (a->n_esteiras * a->n_max_avioes_esteira));
-	sem_init(&a->n_avioes, 0, 0);
-	return a;
-}
+/**
+ * Esta função deve fazer com que o aviao pouse, utilizando uma pista livre.
+ * Ela não pode ser chamada se não houver pistas livres. Em seguida o avião
+ * deve se acoplar a um portão, caso haja um livre, se não houver, ele deve
+ * esperar.
+ **/
+void pousar_aviao (aeroporto_t* aeroporto, aviao_t* aviao);
 
-void aproximacao_aeroporto (aeroporto_t* aeroporto, aviao_t* aviao) {
-	if (aviao->combustivel < 10)
-		inserir(aeroporto->fila_prioritaria, aviao);
-	else
-		inserir(aeroporto->fila_pouso_decolagem, aviao);
+/**
+ * Esta função deve acoplar um avião a um portão de embarque livre.
+ * Ela não deve ser chamada caso não hajam portões livres. Em seguida, o
+ * o avião deve transportar suas bagagens para fora e receber novas bagagens.
+ * Um avião não pode decolar sem receber novas bagagens.
+ **/
+void acoplar_portao (aeroporto_t* aeroporto, aviao_t* aviao);
 
-	sem_post(&aeroporto->n_avioes);  // Sinaliza para a thread aeroporto que um aviao quer a pista
-	printf("Avião %d iniciou aproximação com %d%c de combustivel.\n", (int) aviao->id, (int) aviao->combustivel, 37);
-}
+/**
+ * Esta função deve levar as bagagens de um avião para uma esteira e
+ * colocar novas bagagens num avião para que ele seja liberado para
+ * decolagem. Um avião não pode decolar sem receber novas bagagens.
+ * Um avião não pode receber novas bagagens sem colocar as antigas
+ * em uma esteira.
+ **/
+void transportar_bagagens (aeroporto_t* aeroporto, aviao_t* aviao);
 
-void pousar_aviao (aeroporto_t* aeroporto, aviao_t* aviao) {
-	sem_wait(&aviao->liberacao_pouso_decolagem);
-	remover(aeroporto->fila_pouso_decolagem);
-	usleep(aeroporto->t_pouso_decolagem * MICRO_TO_MILI);
-	printf("Avião %d aterrissou.\n", (int) aviao->id);
-	sem_post(&aeroporto->pistas);
-}
+/**
+ * Esta função deve colocar novas bagagens numa esteira, caso ela
+ * esteja disponível. A esteira ficará ocupada enquanto houverem
+ * bagagens nela.
+ **/
+void adicionar_bagagens_esteira (aeroporto_t* aeroporto, aviao_t* aviao);
 
-void acoplar_portao (aeroporto_t* aeroporto, aviao_t* aviao) {
-	sem_post(&aeroporto->pistas);  // Inverter esta com a proxima se necessario ocupar para desocupar anterior
-	sem_wait(&aeroporto->portoes);
-	printf("Avião %d chegou a um portão de embarque/desembarque.\n", (int) aviao->id);
-}
+/**
+ * Esta função deve fazer com que um avião decole, caso haja
+ * uma pista disponível para tal. Um avião que decolar deve
+ * ter sua execução terminada.
+ **/
+void decolar_aviao (aeroporto_t* aeroporto, aviao_t* aviao);
 
-void transportar_bagagens (aeroporto_t* aeroporto, aviao_t* aviao) {
-	usleep(aeroporto->t_remover_bagagens * MICRO_TO_MILI);
-	printf("Avião %d foi descarregado.\n", (int) aviao->id);
-	adicionar_bagagens_esteira(aeroporto, aviao);  // Ver sobre retirar esta linha
-	usleep(aeroporto->t_inserir_bagagens * MICRO_TO_MILI);
-	printf("Avião %d foi carregado com novas bagagens.\n", (int) aviao->id);
-}
+/**
+ * Esta função deve desalocar todos os recursos previamente
+ * alocados pelo aeroporto. Retorna 1 caso bem sucedido.
+ **/
+int finalizar_aeroporto (aeroporto_t* aeroporto);
 
-void adicionar_bagagens_esteira (aeroporto_t* aeroporto, aviao_t* aviao) {
-	sem_wait(&aeroporto->esteiras);
-	printf("Bagagens do avião %d adicionadas à uma esteira.\n", (int) aviao->id);
-	usleep(aeroporto->t_bagagens_esteira * MICRO_TO_MILI);
-	sem_post(&aeroporto->esteiras);
-}
+/**
+ * Esta função dá permissão para que um avião utilize as pistas
+ * de pouso e decolagem.
+ **/
+void libera_pouso_decolagem(aviao_t* aviao);
 
-void decolar_aviao (aeroporto_t* aeroporto, aviao_t* aviao) {
-	inserir(aeroporto->fila_pouso_decolagem, aviao);
-	sem_wait(&aviao->liberacao_pouso_decolagem);
-	sem_post(&aeroporto->portoes);  // Inverter esta e a proxima se necessario desocupar para ocupar
-	sem_wait(&aeroporto->pistas);
-	remover(aeroporto->fila_pouso_decolagem);
-	usleep(aeroporto->t_pouso_decolagem * MICRO_TO_MILI);
-	printf("Avião %d decolou.\n", (int) aviao->id);
-	sem_post(&aeroporto->pistas);
-}
-
-int finalizar_aeroporto (aeroporto_t* aeroporto) {
-	desaloca_fila(aeroporto->fila_pouso_decolagem);
-	sem_destroy(&(aeroporto->pistas));
-	sem_destroy(&(aeroporto->portoes));
-	sem_destroy(&(aeroporto->esteiras));
-	free(aeroporto);
-
-	return 1;
-}
-
-void libera_pouso_decolagem(aviao_t* aviao) {
-    sem_post(&aviao->liberacao_pouso_decolagem);
-}
+#endif
